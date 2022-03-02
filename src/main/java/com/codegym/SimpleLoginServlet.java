0fd44ddec2d33ff1f82039dfd76da355a0c27309
hoale0507 <hoale090293@gmail.com>
2022-03-02 16:31:01 +0700

    add file

---
+++ src/main/java/com/codegym/SimpleLoginServlet.java
@@ -0,0 +1,29 @@
+package com.codegym;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SimpleLoginServlet", value = "/login")
+public class SimpleLoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write("<html>");
+        if(username.equals("admin") && password.equals("admin")){
+            printWriter.write("<h1>Welcome "+username+" to website</h1>");
+        } else {
+            printWriter.write("<h1>Login Error</h1>");
+        }
+        printWriter.write("</html>");
+    }
+}
